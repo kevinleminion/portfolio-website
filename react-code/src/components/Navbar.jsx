e0667b716c91e674a4e9605a1c0ffd13b61a3ec0
Jordan <jordan.tran2@ucalgary.ca>
2026-07-01 19:33:54 -0600

    created navigation bar, need to test functionality

---
+++ react-code/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Navbar.css';
+
+const NavBar = () => {
+    <nav className = "navbar">
+        <div className = "navbar-container">
+            <a href="/assets/hero.png" >Jordan Tran<span className = "accent">/dev</span></a>
+            <ul className = "navbar-links">
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contacts</a></li>
+            </ul>
+        </div>
+
+    </nav>
+}
+
+export default NavBar;
