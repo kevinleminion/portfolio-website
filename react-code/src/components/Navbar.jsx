f178915e124bf9ff6155965684e69de29b823f18
Jordan <jordan.tran2@ucalgary.ca>
2026-07-03 17:36:07 -0600

    tested some changes to the navbar

--- react-code/src/components/Navbar.jsx
+++ react-code/src/components/Navbar.jsx
@@ -5,14 +5,13 @@ const NavBar = () => {
     return(
     <nav className = "navbar">
         <div className = "navbar-container">
-            <a href="#main" >Jordan Tran<code className = "accent">/dev</code></a>
+            <a href="#main" ><span className = "name">Jordan Tran</span><code className = "accent">.dev</code></a>
             <ul className = "navbar-links">
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contacts</a></li>
             </ul>
         </div>
-
     </nav>
     )
 }
