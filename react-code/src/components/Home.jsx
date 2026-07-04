d18e6ef3a270cb1612d2d9ccd3a7cbbfabff12b9
Jordan <jordan.tran2@ucalgary.ca>
2026-07-03 18:50:12 -0600

    created basis for main page content

---
+++ react-code/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Home.css';
+
+const Home = () => {
+    return (
+        <section className="home-section">
+            <div className="home-container">
+                <div className = "home-content">
+
+                </div>
+
+                <div className="home-image">
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
