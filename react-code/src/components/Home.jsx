4572217f50b9d37c43af305df263a6c210227a4f
Jordan <jordan.tran2@ucalgary.ca>
2026-07-08 22:24:15 -0600

    Messed around with animations for a bit, should begin actual development of the hero soon enough

--- react-code/src/components/Home.jsx
+++ react-code/src/components/Home.jsx
@@ -4,19 +4,23 @@ import jackpotImg from '../assets/jackpot.jpg';
 
 const Home = () => {
     return (
-        <section className="home-section">
-            <div className="home-container">
-                <div className = "home-content">
-                    <h1 className="home-header">
-                        Welcome to My Portfolio
-                    </h1>
-                </div>
-
-                <div className="home-image">
-                    <img src={jackpotImg} alt="Jackpot" />
-                </div>
+        <div className="home-container">
+            <div className = "home-content">
+                <h1 className="home-header">
+                    Welcome to My Portfolio
+                </h1>
             </div>
-        </section>
+
+            <div className = "image-container"> 
+                <figure className="home-image">
+                    <img src={jackpotImg} alt="Jackpot" />
+                    
+                </figure>
+                <h1 className="home-image-header">
+                        "I may be racist, but you were mean and that's worse."
+                </h1>
+            </div>
+        </div>
     );
 };
 
