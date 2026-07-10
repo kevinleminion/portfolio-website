2f5ffad5fbdc119da48894152acb8d095d85bd4f
Jordan <jordan.tran2@ucalgary.ca>
2026-07-09 21:49:31 -0600

    Very good process on homepage, should be done within a few commits.

--- react-code/src/components/Home.jsx
+++ react-code/src/components/Home.jsx
@@ -7,17 +7,24 @@ const Home = () => {
         <div className="home-container">
             <div className = "home-content">
                 <h1 className="home-header">
-                    Welcome to My Portfolio
+                    Welcome, my name is Jordan
                 </h1>
+                <hr className = "separator-line"/> 
+                <p className = "self-intro">
+                    Borderline psychopathic solitude freak, taking life one step at a time.
+                </p>
+                <hr/>
+                <p className = "professional-goals">
+                    Computer science student fascinated about modern cloud infrastructure and the incredible capacity it brings for GitOps automation.
+                </p>
             </div>
 
             <div className = "image-container"> 
                 <figure className="home-image">
                     <img src={jackpotImg} alt="Jackpot" />
-                    
                 </figure>
                 <h1 className="home-image-header">
-                        "I may be racist, but you were mean and that's worse."
+                    "I may be racist, but you were mean and that's worse."
                 </h1>
             </div>
         </div>
