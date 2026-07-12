ab5e67590ed321e9a669208bc78a713d7e899ce1
Jordan <jordan.tran2@ucalgary.ca>
2026-07-11 22:49:01 -0600

    finished base homepage, will come back later to refine

--- react-code/src/components/Home.jsx
+++ react-code/src/components/Home.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import './Home.css';
 import jackpotImg from '../assets/jackpot.jpg';
+import { FaGithub, FaInstagram } from 'react-icons/fa';
 
 const Home = () => {
     return (
         <div className="home-container">
             <div className = "home-content">
                 <h1 className="home-header">
-                    Welcome, my name is Jordan
+                    Welcome to my portfolio site!
                 </h1>
                 <hr className = "separator-line"/> 
                 <p className = "self-intro">
-                    Borderline psychopathic solitude freak, taking life one step at a time.
+                    Exceptionally eccentric solitude freak traversing life one day at a time.
                 </p>
                 <hr/>
                 <p className = "professional-goals">
-                    Computer science student fascinated about modern cloud infrastructure and the incredible capacity it brings for GitOps automation.
+                    I'm a computer science student fascinated about modern cloud infrastructure and the incredible capacity it brings for GitOps automation.
                 </p>
+
+                <div className = "social-links">
+                    <a href = "https://github.com/kevinleminion" target="_blank" rel="noreferrer"> 
+                        <FaGithub className="social-icon" />
+                    </a>
+                    <a href = "https://www.instagram.com/jordantran47/" target="_blank" rel="noreferrer">
+                        <FaInstagram className="social-icon" />
+                    </a>
+                </div>
             </div>
 
             <div className = "image-container"> 
